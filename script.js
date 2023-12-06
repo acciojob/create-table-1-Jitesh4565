3b7f764046a62bd8a86921861f012dcc6e727a65
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-12-06 14:45:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+  const table=document.getElementById('sampleTable');
+   const newRow=table.insertRow(0);
+	var cell1=newRow.insertCell(0);
+	var cell2=newRow.insertCell(1);
+
+	cell1.innerHTML="New Cell1";
+	cell2.innerHTML="New Cell2";
   
 }
